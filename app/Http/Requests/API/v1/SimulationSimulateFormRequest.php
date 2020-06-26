c679979f07642d8cac132a49fc3c117e4f1155cc
Nikolas Timoteo <nikolastps@hotmail.com>
2020-06-26 11:03:51 -0300

    Criação do request do formulario de Simulação

---
+++ app/Http/Requests/API/v1/SimulationSimulateFormRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests\API\v1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SimulationSimulateFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'valor_emprestimo' => 'required|numeric',
+            'instituicoes'     => 'sometimes|nullable|array',
+            'convenios'        => 'sometimes|nullable|array',
+            'parcela'          => 'sometimes|nullable|integer'
+        ];
+    }
+
+    /**
+     * Get the messages to apply to the validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'required'    => 'Campo obrigatório.',
+            'array'       => 'Escolha uma ou mais opções.',
+            'numeric'     => 'Digite um valor numérico.',
+            'integer'     => 'Digite um número inteiro.'
+        ];
+    }
+}
